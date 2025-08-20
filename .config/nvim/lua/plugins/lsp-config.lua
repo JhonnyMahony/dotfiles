c4f3090cfac1eb8db757d64946e001e399bb6d59
JhonnyMahony <rallidars.com>
2025-08-20 15:31:20 +0300

    some rust html changes

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     init_options = {
     },
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
     --url = 'git@github.com:neovim/nvim-lspconfig.git',
-		lazy = false,
+    lazy = false,
     init_options = {
       userLanguages = {
         eelixir = "html-eex",
@@ -26,15 +26,6 @@ return {
         rust = "html",
       },
     },
-        settings = {
-          ["rust-analyzer"] = {
-            cargo = {
-              extraEnv = {
-                RUSTFLAGS = "--cfg=web_sys_unstable_apis",
-              },
-            },
-          },
-        },
 
     vim.diagnostic.config({
       virtual_text = true,
@@ -46,38 +37,37 @@ return {
     }),
 
 
-		config = function()
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
       })
 
       lspconfig.ts_ls.setup({
-				capabilities = capabilities,
+        capabilities = capabilities,
       })
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-				filetypes = { "html", "rs" },
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "rs", "rust" },
+      })
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
 
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
